7690f550de596030f51e990d495827cf587ebd8a
Andrey Slepukhov <a668776@yandex.ru>
2023-02-02 20:24:16 +0300

    Stub and tests for add

--- src/main/PhoneBook.java
+++ src/main/PhoneBook.java
@@ -2,4 +2,13 @@ package main;
 
 public class PhoneBook {
 
+    private PhoneBook() {
+    }
+
+    public int add(String name, String number) {
+
+        return 0;
+    }
+
+
 }
--- src/tests/PhoneBookTest.java
+++ src/tests/PhoneBookTest.java
@@ -1,5 +1,27 @@
 package tests;
 
+import main.PhoneBook;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @Test
+    public void testAdd() {
+        //assert
+        int expected = 2;
+        int result;
+
+        //act
+
+        phoneBook.add("Ivan", "89123456789");
+        result = phoneBook.add("Masha", "8987654321");
+
+        //assert
+        assertEquals(expected, result);
+    }
+
 
 }
